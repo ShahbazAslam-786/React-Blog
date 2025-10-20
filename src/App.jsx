a1362dc26f6dd42b2a6b0b8c4c3fe3432bcc66b7
ShahbazAslam-786 <zunnurainbuilders@gmail.com>
2025-10-20 17:22:11 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App () {
+  const userName="Anil Sidhu"
+  let x=20
+  let y=30
+  return(
+    <>
+    <h1>JSX in React.jS</h1>
+    <h1>{userName}</h1>
+    <h1>{10+20+30}</h1>
+    <h1>{x*y}</h1>
+    </>
+  )
+}
+
+export default App;
---
+++ src/UserComponent.jsx
@@ -0,0 +1,28 @@
+function Login () {
+    return (
+        <div>
+            <h1>User Login</h1>
+        </div>
+    )
+}
+
+export default Login;
+
+export function Profile () {
+    return(
+        <div>
+            <h1>User Profile</h1>
+        </div>
+    )
+}
+
+
+export function Setting () {
+    return(
+        <div>
+            <h1>User Setting</h1>
+        </div>
+    )
+}
+
+export const Userkey ="Anil Sidhu"
