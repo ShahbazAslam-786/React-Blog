f21c042e6e629542117198278ec1e3ea2e5afd4b
ShahbazAslam-786 <zunnurainbuilders@gmail.com>
2025-11-12 22:43:19 +0500

    Multiple Condition In React Js

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import { useState } from "react";
-import User from "./User";
 
-function App () {
-  const [display, setDisplay]= useState(true)
-  return(
-    <>
-    <h1>Toggle In React Js</h1>
-    <button onClick={()=>setDisplay(!display)}>Toggle</button>
-    {
-      display ? <User />: null
-    }
-    </>
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+    <div>
+      <h1>Multiple Condition In React Js</h1>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>Counter</button>
+      {
+        count == 0 ? <h1>Condition 0</h1>
+        :count==1? <h1>Condition 1</h1> 
+        :count==2? <h1>Condition 2</h1> 
+        :count==3? <h1>Condition 3</h1> 
+        :count==4? <h1>Condition 4</h1> 
+        // :null
+        :<h1>Other Condition</h1>
+      }
+    </div>
   )
 }
 
--- src/User.jsx
+++
@@ -1,9 +0,0 @@
-function User () {
-    return (
-        <div>
-            <h1>Anil Sidhu</h1>
-        </div>
-    )
-}
-
-export default User;
