4dd658f9e028ed9d67237ab56c4ea7c6bc4b3904
ShahbazAslam-786 <zunnurainbuilders@gmail.com>
2025-11-15 00:42:16 +0500

    Get Input Field Value

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from "react";
+import { useState } from "react"
 
 function App () {
   const [val, setVal] = useState("")
   return (
     <div>
       <h1>Get Input Field Value</h1>
-      <input type="text" value={val} onChange={(event)=> setVal(event.target.value)} placeholder="Enter User Name" />
+      <input type="text" value={val} onChange={(event) => setVal(event.target.value)} placeholder="Enter User Name" />
       <h1>{val}</h1>
-      <button onClick={() => setVal("")}>Clear Value</button> 
+      <button onClick={()=> setVal("")}>Clear Value</button>
     </div>
   )
 }
