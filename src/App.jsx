f02e75a5cb22e0242ccb3483191e5010ba60ebe2
ShahbazAslam-786 <zunnurainbuilders@gmail.com>
2025-12-06 11:35:08 +0500

    dynamic conditional style in react js

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,129 @@
 import { useState } from "react";
-import Counter from "./Counter";
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [data, setData] = useState(0)
-  const [display, setDisplay] = useState(true)
+
+  const [cardStyle, setCardStyle] = useState({
+    border: "1px solid #ccc",
+    width: "200px",
+    boxShadow: "1px 2px 3px 0px #ccc",
+    margin: "10px"
+  })
+
+  const [textColor, setTextColor] = useState("black")
+  const [grid, setGrid] = useState(true)
+
+  const updateTheme = (bgColor, textColor) => {
+    // console.log(bgColor, textColor)
+    setCardStyle({ ...cardStyle, backgroundColor: bgColor })
+    setTextColor(textColor)
+  }
   return (
-    <div>
-      {
-        display ? <Counter count={count} data={data}/>:null
-      }
-      <button onClick={() => setCount(count + 1)}>Counter</button>
-      <button onClick={() => setData(data + 1)}>Data</button>
-      <button onClick={() => setDisplay(!display)}>Toggle</button>
-    </div>
+    <>
+      <h1 style={{ color: "#000" }}>Dynamic Condional Style In React Js</h1>
+
+      <button onClick={() => updateTheme("#ccc", "red")}>Gray Theme</button>
+      <button onClick={() => updateTheme("white", "black")}>Default Theme</button>
+      <button onClick={() => setGrid(!grid)}>Toggle Grid</button>
+
+      <div style={{ display: grid ? "flex":"block", flexWrap: "wrap" }}>
+
+        <div style={cardStyle}>
+          <img style={{ width: "200px" }} src="https://api.brusselstimes.com/wp-content/uploads/2019/05/vddriessche-c-stamp-media.jpg" alt="" />
+          <div style={{ padding: "5px", color: textColor }}>
+            <h2>Anil Sidhu</h2>
+            <p>Softwear Developer</p>
+          </div>
+        </div>
+        
+        <div style={cardStyle}>
+          <img style={{ width: "200px" }} src="https://api.brusselstimes.com/wp-content/uploads/2019/05/vddriessche-c-stamp-media.jpg" alt="" />
+          <div style={{ padding: "5px", color: textColor }}>
+            <h2>Anil Sidhu</h2>
+            <p>Softwear Developer</p>
+          </div>
+        </div>
+
+        <div style={cardStyle}>
+          <img style={{ width: "200px" }} src="https://api.brusselstimes.com/wp-content/uploads/2019/05/vddriessche-c-stamp-media.jpg" alt="" />
+          <div style={{ padding: "5px", color: textColor }}>
+            <h2>Anil Sidhu</h2>
+            <p>Softwear Developer</p>
+          </div>
+        </div>
+
+        <div style={cardStyle}>
+          <img style={{ width: "200px" }} src="https://api.brusselstimes.com/wp-content/uploads/2019/05/vddriessche-c-stamp-media.jpg" alt="" />
+          <div style={{ padding: "5px", color: textColor }}>
+            <h2>Anil Sidhu</h2>
+            <p>Softwear Developer</p>
+          </div>
+        </div>
+
+        <div style={cardStyle}>
+          <img style={{ width: "200px" }} src="https://api.brusselstimes.com/wp-content/uploads/2019/05/vddriessche-c-stamp-media.jpg" alt="" />
+          <div style={{ padding: "5px", color: textColor }}>
+            <h2>Anil Sidhu</h2>
+            <p>Softwear Developer</p>
+          </div>
+        </div>
+
+        <div style={cardStyle}>
+          <img style={{ width: "200px" }} src="https://api.brusselstimes.com/wp-content/uploads/2019/05/vddriessche-c-stamp-media.jpg" alt="" />
+          <div style={{ padding: "5px", color: textColor }}>
+            <h2>Anil Sidhu</h2>
+            <p>Softwear Developer</p>
+          </div>
+        </div>
+
+        <div style={cardStyle}>
+          <img style={{ width: "200px" }} src="https://api.brusselstimes.com/wp-content/uploads/2019/05/vddriessche-c-stamp-media.jpg" alt="" />
+          <div style={{ padding: "5px", color: textColor }}>
+            <h2>Anil Sidhu</h2>
+            <p>Softwear Developer</p>
+          </div>
+        </div>
+
+        <div style={cardStyle}>
+          <img style={{ width: "200px" }} src="https://api.brusselstimes.com/wp-content/uploads/2019/05/vddriessche-c-stamp-media.jpg" alt="" />
+          <div style={{ padding: "5px", color: textColor }}>
+            <h2>Anil Sidhu</h2>
+            <p>Softwear Developer</p>
+          </div>
+        </div>
+
+        <div style={cardStyle}>
+          <img style={{ width: "200px" }} src="https://api.brusselstimes.com/wp-content/uploads/2019/05/vddriessche-c-stamp-media.jpg" alt="" />
+          <div style={{ padding: "5px", color: textColor }}>
+            <h2>Anil Sidhu</h2>
+            <p>Softwear Developer</p>
+          </div>
+        </div>
+
+        <div style={cardStyle}>
+          <img style={{ width: "200px" }} src="https://api.brusselstimes.com/wp-content/uploads/2019/05/vddriessche-c-stamp-media.jpg" alt="" />
+          <div style={{ padding: "5px", color: textColor }}>
+            <h2>Anil Sidhu</h2>
+            <p>Softwear Developer</p>
+          </div>
+        </div>
+
+        <div style={cardStyle}>
+          <img style={{ width: "200px" }} src="https://api.brusselstimes.com/wp-content/uploads/2019/05/vddriessche-c-stamp-media.jpg" alt="" />
+          <div style={{ padding: "5px", color: textColor }}>
+            <h2>Anil Sidhu</h2>
+            <p>Softwear Developer</p>
+          </div>
+        </div>
+
+        <div style={cardStyle}>
+          <img style={{ width: "200px" }} src="https://api.brusselstimes.com/wp-content/uploads/2019/05/vddriessche-c-stamp-media.jpg" alt="" />
+          <div style={{ padding: "5px", color: textColor }}>
+            <h2>Anil Sidhu</h2>
+            <p>Softwear Developer</p>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
