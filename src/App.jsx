65755446ba60cfe7704800ecc7b15c3bf9a37333
ShahbazAslam-786 <zunnurainbuilders@gmail.com>
2025-12-10 10:26:31 +0500

    styled component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,46 @@
-import UserProfile from "./userProfile";
-// import "./Css/style.css";
+import styled from "styled-components";
 
-function App() {
+function App () {
+
+  // const Heading =styled.h1`
+  // color:red;
+  // border: 1px solid green;
+  // border-radius: 5px;
+  // margin: 20px;
+  // padding: 10px;
+  // `
+
+  const Heading = styled.h1({
+    color: "green",
+    border: "1px solid #000",
+    borderRadius: "5px",
+    margin: "20px",
+    padding: "10px",
+    width: "200px"
+  })
+
+  const StyleBtn = styled.button `
+  color: red ;
+  width: 130px;
+  height: 40px;
+  margin: 20px;
+  `
+
+  const Para = styled.p `
+  color: blue;
+  `
   return (
     <>
-      <h1 className="heading">Style With Css Module In React jS</h1>
-      <div style={{display:"flex", flexWrap: "wrap"}}>
-        <UserProfile />
-        <UserProfile />
-        <UserProfile />
-        <UserProfile />
-        <UserProfile />
-        <UserProfile />
-        <UserProfile />
-        <UserProfile />
-        <UserProfile />
-        <UserProfile />
-        <UserProfile />
-        <UserProfile />
-      </div>
+    <h1>Styled Component With React jS</h1>
+    <Heading>Hello Heading</Heading>
+    <Heading>Hello Heading</Heading>
+    <Heading>Hello Heading</Heading>
+    <Heading>Hello Heading</Heading>
+    <StyleBtn>Login</StyleBtn>
+    <StyleBtn>SignUp</StyleBtn>
+    <StyleBtn>Back</StyleBtn>
+    <StyleBtn>Forward</StyleBtn>
+     <Para>Hello Anil Sidhu</Para>
     </>
   )
 }
