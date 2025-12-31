58aa09d8ad83e6186e38d53c212ad1817beb30a1
ShahbazAslam-786 <zunnurainbuilders@gmail.com>
2025-12-31 11:17:22 +0500

    use fragment

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,22 @@
-import { useId } from "react";
-
- export default function App() {
-
+export default function App () {
     return (
         <>
-            {/* <h1>useId Hook</h1> */}
-            {/* <h1>{name}</h1>
-            <h1>{password}</h1>
-            <h1>{terms}</h1>
-            <h1>{skills}</h1> */}
-
-            {/* <form action="">
-                <label htmlFor="name">enter user name</label>
-                <input id="name" type="text" placeholder="enter name" />
-            </form> */}
-            <UserForm/>
-            <hr />
-            <UserForm/>
+        <Data/>
+        <Data/>
+        <Data/>
+        <Data/>
         </>
     )
 }
 
-function UserForm () {
-    //  let name = useId()
-    // let password = useId()
-    // let terms = useId()
-    // let skills = useId()
-    let user =useId()
-    return (
+function Data () {
+    return(
+        // <>
+        // <h1>Fargment in React js</h1>
+        // <h1>Fargment in React js</h1>
+        // </>
         <div>
-         <form action="">
-                <label htmlFor={user+"name"}>Enter User Name</label>
-                <input id={user+"name"} type="text" placeholder="enter name" />
-                <br /><br />
-                <label htmlFor={user+"password"}>Enter User password</label>
-                <input id={user+"password"} type="text" placeholder="enter password" />
-                <br /><br />
-                <label htmlFor={user+"skills"}>Enter User skills</label>
-                <input id={user+"skills"} type="text" placeholder="enter skills" />
-                 <br /><br />
-                <label htmlFor={user+"terms"}>Terms and Condition</label>
-                <input id={user+"terms"} type="checkbox" placeholder="enter terms" />
-            </form>
-            </div>
+            <span>Hello</span><b>user</b>
+        </div>
     )
 }
-
- 
